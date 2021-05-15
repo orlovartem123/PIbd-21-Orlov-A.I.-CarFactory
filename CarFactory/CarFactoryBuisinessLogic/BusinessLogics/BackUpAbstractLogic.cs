fc85e18aee80b15a6f5222491c60a4861776bfef
orlovartem123 <orlovartem123@gmail.com>
2021-05-16 03:07:28 +0400

    remove json

--- CarFactory/CarFactoryBuisinessLogic/BusinessLogics/BackUpAbstractLogic.cs
+++ CarFactory/CarFactoryBuisinessLogic/BusinessLogics/BackUpAbstractLogic.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Serialization.Json;
 using System.Xml.Linq;
 
 namespace CarFactoryBusinessLogic.BusinessLogics
@@ -52,18 +51,6 @@ namespace CarFactoryBusinessLogic.BusinessLogics
             {
                 // делаем проброс
                 throw;
-            }
-        }
-        private void SaveToFile<T>(string folderName) where T : class, new()
-        {
-            var records = GetList<T>();
-            T obj = new T();
-            DataContractJsonSerializer jsonFormatter = new
-           DataContractJsonSerializer(typeof(List<T>));
-            using (FileStream fs = new FileStream(string.Format("{0}/{1}.json",
-           folderName, obj.GetType().Name), FileMode.OpenOrCreate))
-            {
-                jsonFormatter.WriteObject(fs, records);
             }
         }
 
